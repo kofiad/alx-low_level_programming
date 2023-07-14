18eb2e75ba1ece55904fdf82366f1ed466cea698
kofiad <kofiadaba@gmail.com>
2023-07-14 23:59:43 +0000

    4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@ int main(void)
 	char alphabet;
 
 	/*loop to print alphabet in lowercase minus q,e*/
-	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		if(alphabet != 'q' && alphabet != 'e')
+		if (alphabet != 'q' && alphabet != 'e')
 		{
 			putchar(alphabet);
 		}
 	}
 	putchar('\n');
-return(0);
+return (0);
 }
