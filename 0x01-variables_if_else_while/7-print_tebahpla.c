71d314eab0e8fefde059830b93255c87acf82c9c
kofiad <kofiadaba@gmail.com>
2023-07-15 00:25:01 +0000

    7. Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,7 +9,7 @@ int main(void)
 	char alphabet;
 
 	/*print alphabet in lowercase in reverse*/
-	for (alphabet = 'z'; alphabet <= 'a'; alphabet--)
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
 	{
 		putchar(alphabet);
 	}
