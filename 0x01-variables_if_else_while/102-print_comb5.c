2985891a4fb95920fc011a1982f8f53f81184439
kofiad <kofiadaba@gmail.com>
2023-07-15 02:28:45 +0000

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,22 +9,22 @@ int main(void)
 {
     int g, i;
 
-    for (g = 0; g <= 99; g++) 
-    {
-        for (i = g; i <= 99; i++) 
+	for (g = 0; g <= 99; g++) 
 	{
-            putchar(g / 10 + '0');
-            putchar(g % 10 + '0');
-            putchar(' ');
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            if (g != 99 || i != 99) 
-	    {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
+		for (i = g; i <= 99; i++) 
+		{
+		putchar(g / 10 + '0');
+		putchar(g % 10 + '0');
+		putchar(' ');
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+			if (g != 99 || i != 99)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 return 0;
 }
