59ed1473ac3f16bdade3aca677ef63312623cee1
kofiad <kofiadaba@gmail.com>
2023-07-15 00:08:03 +0000

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print out 1-9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	/*printing out 1-9*/
+	for (num = '1';num < '10'; num++)
+	{putchar(num);
+	}
+	putchar('\n');
+return (0);
+}
