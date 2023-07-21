ffe5f64d90ae8e68a7ec1689b5bcdcddbc37528d
kofiad <kofiadaba@gmail.com>
2023-07-21 18:54:04 +0000

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,44 @@
 #include <stdio.h>
+/**
+ * fizz_buzz - prints numbers replacing
+ * multiples of 3 with Fizz
+ * multiples of 5 with Buzz
+ * multiples of both FizzBuzz
+ *
+ * Return: void
+ */
 
 void fizz_buzz(void)
 {
 	int num;
-    for (num = 1; num <= 100; num++)
-    {
-        if (num % 3 == 0 && num % 5 == 0)
-        {
-            printf("FizzBuzz ");
-        }
-        else if (num % 3 == 0)
-        {
-            printf("Fizz ");
-        }
-        else if (num % 5 == 0)
-        {
-            printf("Buzz ");
-        }
-        else
-        {
-            printf("%d ", num);
-        }
-    }
-}
 
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+		printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+}
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    fizz_buzz();
-    return 0;
+	fizz_buzz();
+	return (0);
 }
