4743474d3ac3db6d2c236e3d8fb21c03b24b7114
kofiad <kofiadaba@gmail.com>
2023-07-21 17:21:57 +0000

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 ==0)
+		{
+			printf("FizzBuzz\t");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz\t");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz\t");
-		}
-		else if (i % 5 == 0 && i % 3 == 0)
-		{
-			printf("FizzBuzz\t");
 		}
 		else
 		{
