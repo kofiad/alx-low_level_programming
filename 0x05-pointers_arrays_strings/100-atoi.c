8fcca6015315200eedaad3240bf28f1a04528e3f
kofiad <kofiadaba@gmail.com>
2023-07-25 17:08:10 +0000

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,46 +1,50 @@
+#include <stdio.h>
 #include <limits.h>
 
 /**
- * _atoi - Converts a string to an integer.
- *
+ * _atoi - Convert a string to an integer.
  * @s: The input string.
  *
- * Return: The converted integer value.
+ * Return: The converted integer.
  */
 int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
-	int i = 0;
+	int digit;
 
-	while (s[i] != '\0')
+	/* Skip leading whitespaces */
+	while (*s == ' ')
+		s++;
+
+	/* Check the sign */
+	if (*s == '-')
 	{
-		if (s[i] == '-')
-		{
-			sign = -sign;
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			int digit = s[i] - '0';
-
-			/*Check for integer overflow*/
-			if (result > INT_MAX / 10 ||
-					(result == INT_MAX / 10 &&
-					 digit > INT_MAX % 10))
-			{
-				/*Return INT_MAX for positive values*/
-				return ((sign == 1) ? INT_MAX : INT_MIN);
-			}
-
-			result = result * 10 + digit;
-		}
-		else if (result != 0)
-		{
-			break;
-		}
-
-		i++;
+		sign = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
 	}
 
-	return (result * sign);
+	/* Convert the string to an integer */
+	while (*s >= '0' && *s <= '9')
+	{
+		digit = *s - '0';
+
+		/* Check for potential overflow */
+		if (result > (INT_MAX - digit) / 10)
+		{
+			if (sign == 1)
+				return INT_MAX;
+			else
+				return INT_MIN;
+		}
+
+		result = result * 10 + digit;
+		s++;
+	}
+
+	return result * sign;
 }
