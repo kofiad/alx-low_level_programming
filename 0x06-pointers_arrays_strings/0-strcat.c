87b6c5202b1fcf545b50bb55c91dbc4b61e78f02
kofiad <kofiadaba@gmail.com>
2023-07-28 20:09:55 +0000

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * string_length - finds length of a string
+ * 
+ * @str: string whose length is to be determined
+ * @dest: first string for concatenation
+ * @src: second string for concatenation
+ *
+ * Returns: length,
+ * *dest for _strcat
+ */
+size_t string_length(const char *str)
+{
+	size_t length = 0;
+	while (*str != '\0')
+	{
+
+}
+char *_strcat(char *dest, char *src)
+{
+
+}
