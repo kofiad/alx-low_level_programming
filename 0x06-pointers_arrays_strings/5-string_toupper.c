52ab5ea81c85d2fda3b73ea5175a33fe396c15cc
kofiad <kofiadaba@gmail.com>
2023-07-28 23:55:00 +0000

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes all lowercase to uppercase
+ *
+ *@l: string for conversion
+ *
+ * Return: pointer to uppercase string
+ */
+char *string_toupper(char *l)
+{
+	int length = 0;
+
+	while (l[length] != '\0')
+	{
+		if (l[length >= 97 && l[length] <= 122)
+		{
+			l[length] = l[length] - 32;
+		}
+		length++;
+	}
+	return (l)
+}
