6a111d0ca7523e902c7c5150da175d24b35a0acc
kofiad <kofiadaba@gmail.com>
2023-07-28 21:31:20 +0000

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,48 +1,31 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates two strings
- * string_length - finds length of a string
- * 
- * @str: string whose length is to be determined
+ * _strcat - concatenates two strings
+ *
  * @dest: destination string for concatenation
  * @src: source string for concatenation
  *
- * Returns: length,
- * *dest for _strcat
+ * Return: *dest
  */
-/*fuction to detemine length of string*/
-size_t string_length(const char *str)
-{
-	size_t length = 0;
-	while (*str != '\0')
-	{
-		length++;
-		str++;
-	}
-return (length);
-}
-/*fuction to concatenate two strings*/
 char *_strcat(char *dest, char *src)
 {
-	char *destination_start =  dest;
-	size_t destination_length = string_length(dest);
-	size_t source_length = string_length(src);
+	int d;
+	size_t length;
 
-	/*moving pointer to end of string*/
-	dest += destination_length;
-
-	/*copying characters from source to destination
-	 * till end of source string is reached
-	 */
-	while (*src != '\0')
+	/*verify destination arrat is not null*/
+	while (dest[length] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		length++;
+	}
+
+	/*innitialize for loop to concatenate two strings*/
+	for (d = 0; src[d] != '\0'; d++; length++)
+	{
+		dest[length] = src[d];
 	}
 
 	/*add terminating null byte at the end*/
-	*dest = '\0';
-return (destination_start);
+	dest[length] = '\0';
+return (dest);
 }
