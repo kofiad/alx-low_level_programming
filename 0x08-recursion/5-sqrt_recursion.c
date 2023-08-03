6592ceb8c22d6a50c978e37aaf71bb9e6b15b317
kofiad <kofiadaba@gmail.com>
2023-08-03 03:38:17 +0000

    5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion_checker - checks for natural square root of a number
+ *
+ * @n: number of interest
+ * @start: starting number
+ * @end: ending number
+ *
+ * Return: -1 if number has no natural square root
+ * natural square root
+ */
+int _sqrt_recursion_checker(int n, int start, int end)
+{
+	int mid, square;
+
+	if (start <= end)
+	{
+		mid = start + (end - start) / 2;
+		square = mid * mid;
+		if (square == n)
+		{
+			return (mid);
+		}
+		else if (square < n)
+		{
+			return (_sqrt_recursion_checker(n, mid + 1, end));
+		}
+		else
+		{
+		return (_sqrt_recursion_checker(n, start, mid - 1));
+		}
+	}
+}
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ *
+ * @n: number of interest
+ *
+ * Return: -1 if no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if n < 0)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursion_checker(n, 0, n);
+}
