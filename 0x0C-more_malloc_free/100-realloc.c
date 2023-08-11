a9a68a5566df2df7f29a765967ea4ec07ec13977
kofiad <kofiadaba@gmail.com>
2023-08-11 17:07:51 +0000

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		j = malloc(new_size);
 		return (j);
 	}
-	else if (new_size ==0)
+	else if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	j = malloc(new_size);
 	if (j == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	if (new_size > old_size)
 	{
