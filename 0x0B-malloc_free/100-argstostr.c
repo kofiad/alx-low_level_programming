d6918f323db58d55ef1c89899300cf4369b4d5c9
kofiad <kofiadaba@gmail.com>
2023-09-06 00:29:43 +0000

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all argument of my program
+ *
+ * @ac: argument count
+ * @av: argument value
+ *
+ * Return: pointer to a new string
+ * NULL on failure
+ */
+char *argstostr(int ac, char **av)
+{
+	char *result;
+	int totalLength = 0;
+	int i = 0, j = 0, k=0; 
+
+	/*return NULL if av or ac is NULL*/
+	if (ac == NULL || av == NULL)
+	{
+		return (NULL);
+	}
+	/*Calculate total length needed for concateneated string*/
+	for (i = 0; i < ac; i++)
+	{
+		while (av[i][j] != '\0')
+		{
+			totalLength++;
+			j++;
+		}
+		/*creating newline character*/
+		totalLength++;
+	}
+	/*allocating memory for concatenated string*/
+	result = (char *)malloc((totalLength + 1) * sizeof(char));
+	if (result == NULL)
+	{/*return NULL on memory allocation failure*/
+		return (NULL);
+	}
+	/*Copy each argument followed by newline character to the result*/
+	for (i = 0; i < ac; i++)
+	{
+		while (av[i][j] != '\0')
+		{
+			result[k] = av[i][j];
+			k++;
+			j++;
+		}
+		result[k] = '\n';/*adds newline character*/
+		k++;
+	}
+	result[k] = '\0';/*add NULL terminator at the end*/
+	return (result);
+}
